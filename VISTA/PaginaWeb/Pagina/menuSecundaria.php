c7c4864dfd0e5d09bdc4cf85c9c6d477ab5ab07e
Emiliano Acquistapace <emilianoacqui@gmail.com>
2025-10-27 00:24:50 -0300

    prueba arreglo de bachillerato

--- VISTA/PaginaWeb/Pagina/menuSecundaria.php
+++ VISTA/PaginaWeb/Pagina/menuSecundaria.php
@@ -92,7 +92,7 @@
                         <h3><?php echo $ms['pc_h'][$cl]; ?></h3>
                         <p><?php echo $ms['pc_p'][$cl]; ?></p>
                         
-                            <a href="segundoCiclo.php" class="program-button" style="display: inline-block; text-decoration: none;">
+                            <a href="Bachillerato.php" class="program-button" style="display: inline-block; text-decoration: none;">
     <?php echo $ms['see_program'][$cl]; ?>
 </a>
 
